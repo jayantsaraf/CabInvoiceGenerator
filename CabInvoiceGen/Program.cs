c26342bcf108d8c58ef68191750bf7eede80b6bb
jayantsaraf <sarafjayant24@gmail.com>
2020-10-30 15:55:58 +0530

    [jayant]Add. added welcome files" git commit -m"[jayant]Add. added welcome files"

---
+++ CabInvoiceGen/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CabInvoiceGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
