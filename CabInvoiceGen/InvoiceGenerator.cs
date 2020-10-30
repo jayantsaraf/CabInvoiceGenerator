a9652b0e090580cdf556dfb596f3d0b52e306fb2
jayantsaraf <sarafjayant24@gmail.com>
2020-10-30 16:11:00 +0530

    [jayant]Add. added function Calcualte Fare and Test case

---
+++ CabInvoiceGen/InvoiceGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CabInvoiceGen
+{
+    public class InvoiceGenerator
+    {
+        /// <summary>
+        /// Function to calculate fare
+        /// </summary>
+        /// <param name="distance"></param>
+        /// <param name="time"></param>
+        /// <returns></returns>
+        public double CalculateFare(double distance, int time)
+        {
+            int minimumFare = 5;
+            int farePerKM = 10;
+            int farePerMinute = 1;
+            double totalFare = minimumFare + farePerKM * distance + farePerMinute* time;
+            return totalFare;
+        }
+    }
+}
